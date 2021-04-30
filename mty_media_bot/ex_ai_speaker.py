f4c8b5375a4b96da44708c258bac98a3f33c3fb0
assodium@naver.com <assodium@naver.com>
2021-05-01 01:18:04 +0900

    '4th'

--- mty_media_bot/ex_ai_speaker.py
+++ mty_media_bot/ex_ai_speaker.py
@@ -25,26 +25,12 @@ msg = '명령하세요'
 Saying(msg)
 
 """Part 2 : Speech Recognition from Mic."""
-# print(sr.Microphone.list_microphone_names())
+print(sr.Microphone.list_microphone_names())
 mic = sr.Microphone(device_index=1) #'Microsoft ???? - Input'
 with mic as source:
     r.adjust_for_ambient_noise(source)
     audio = r.listen(source)
-"""
-playsound('rfr.wav')
 
-file_input = sr.AudioFile('rfr.wav')
-with file_input as source:
-    # r.adjust_for_ambient_noise(source)
-    audio = r.record(source)
-# fs = 44100  # Sample rate
-# seconds = 3  # Duration of recording
-
-# myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
-# sd.wait()  # Wait until recording is finished
-# write('output.wav', fs, myrecording)  # Save as WAV file 
-"""
-res = r.recognize_sphinx(audio, language="ko-KR") #'ko-KR' en-US
-# res = r.recognize_google(audio, language="ko-KR") #'ko-KR' en-US
+res = r.recognize_google(audio, language="ko-KR") #'ko-KR' en-US
 print('Result : ', res)
 Saying(res)
