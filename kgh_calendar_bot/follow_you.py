6ea8afd6f55880ce0e6405f0e96f75c8359e685b
jubil2gh <assodium@naver.com>
2021-05-12 02:54:33 +0900

    21.0512.0030

--- kgh_calendar_bot/follow_you.py
+++ kgh_calendar_bot/follow_you.py
@@ -5,9 +5,20 @@ engine.setProperty('rate', 170)
 #print(sr.Microphone.list_microphone_names())
 
 def Saying(msg):
-    print(msg)
+    print('TTS: ', msg)
     engine.say(msg)
     engine.runAndWait()
+    
+def speak2txt(audio):
+    try:
+        txt = r.recognize_google(audio, language="ko-KR") #'ko-KR' en-US
+    except sr.RequestError:
+        txt = 'Fail'
+        print('Translate Fail')
+    except sr.UnknownValueError:
+        txt = 'Fail'
+        print('Translate Fail')
+    return txt
 
 r = sr.Recognizer()
 mic = sr.Microphone(device_index=2) 
@@ -18,12 +29,26 @@ while True:
         r.adjust_for_ambient_noise(source)
         audio = r.listen(source)
     Saying('On process')
+    
+    while speak2txt(audio) == 'Fail':
+        print('wait voice..')
+        with mic as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
 
     with open("mic-results.wav", "wb") as f:
         f.write(audio.get_wav_data())
     Saying('Saved your word')
-
+    
     res = r.recognize_google(audio, language="ko-KR") #'ko-KR' en-US
+    while len(res) == 0:
+        Saying('I will follow you')
+        with mic as source:
+            r.adjust_for_ambient_noise(source)
+            audio = r.listen(source)
+        Saying('On process')
+        res = r.recognize_google(audio, language="ko-KR") #'ko-KR' en-US
+        
     print('Result : ', res)
     Saying(res)
 
